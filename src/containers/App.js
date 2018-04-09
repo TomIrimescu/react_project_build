82e7ddd3896b534dd9bb3e3a8b20af5fa3e6005e
TomIrimescu <tom@irimescu.com>
2018-04-09 11:30:18 -0400

    Change App.js, Persons.js to Pure Components & added update lifecycle hooks

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,10 +1,10 @@
-import React, { Component } from "react";
+import React, { PureComponent } from "react";
 
 import AppStyle from "./App.css";
 import Persons from "../components/Persons/Persons";
 import Cockpit from "../components/Cockpit/Cockpit";
 
-class App extends Component {
+class App extends PureComponent {
   constructor(props) {
     super(props);
     // console.log('Log inside constructor [props.title]: ' + props.title);
@@ -37,6 +37,29 @@ class App extends Component {
 
   componentDidMount() {
     console.log("[App.js] Inside componentDidMount()");
+  }
+  
+/*  shouldComponentUpdate(nextProps, nextState) {
+    console.log(
+      "[UPDATE App.js] Inside shouldComponentUpdate",
+      nextProps,
+      nextState
+    );
+    return nextState.persons !== this.state.persons ||
+      nextState.showPersons !== this.state.showPersons;
+    // return true;
+  }*/
+  
+  componentWillUpdate(nextProps, nextState) {
+    console.log(
+      "[UPDATE App.js] Inside componentWillUpdate",
+      nextProps,
+      nextState
+    );
+  }
+  
+  componentDidUpdate() {
+    console.log("[UPDATE App.js] Inside componentDidUpdate");
   }
 
   nameChangedHandler = (event, id) => {
@@ -88,6 +111,7 @@ class App extends Component {
 
     return (
       <div className={AppStyle.App}>
+        <button onClick={() => {this.setState({showPersons: true})}}>Show Persons</button>
         <Cockpit
           appTitle={this.props.title}
           showPersons={this.state.showPersons}
